2e1824fccc5b24e8e046bf15363af0c47a2dcbd0
DashaPC <miss.sinkova-dasha@yandex.ru>
2021-09-13 22:26:00 +0300

    Обновление

--- src/main/java/db/GenerateExpression.java
+++ src/main/java/db/GenerateExpression.java
@@ -5,11 +5,11 @@ import java.time.format.DateTimeFormatter;
 
 public class GenerateExpression {
     public static void main(String[] args) {
-        LocalDateTime date = LocalDateTime.parse("2021-09-23T00:31:13");
-        //LocalDateTime date = LocalDateTime.now();
+        //LocalDateTime date = LocalDateTime.parse("2021-09-23T00:31:13");
+        LocalDateTime date = LocalDateTime.now();
         for (int i = 0; i < 10; i++) {
             System.out.println("insert into incidents(title, created) values ('WAN Балтика', '" + date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "');");
-            date = date.plusDays(1);
+            date = date.plusHours(1);
         }
     }
 }
