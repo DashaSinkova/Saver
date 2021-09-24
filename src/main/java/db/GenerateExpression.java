cc9d7bdf784e6d12c542ecbb2effea163c514f78
DashaPC <miss.sinkova-dasha@yandex.ru>
2021-09-24 22:04:56 +0300

    Презентация

--- src/main/java/db/GenerateExpression.java
+++ src/main/java/db/GenerateExpression.java
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 public class GenerateExpression {
     public static void main(String[] args) {
-        //LocalDateTime date = LocalDateTime.parse("2021-09-23T00:31:13");
-        LocalDateTime date = LocalDateTime.now();
+        LocalDateTime date = LocalDateTime.parse("2021-09-14T20:22:22");
+       // LocalDateTime date = LocalDateTime.now();
         for (int i = 0; i < 10; i++) {
             System.out.println("insert into incidents(title, created) values ('WAN Балтика', '" + date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "');");
             date = date.plusHours(1);
